f02d88e7ee1224ed1e8953f6b5d02d06a91af528
Giovanni Longobucco <giovanni.longobucco@arcoda.it>
2022-01-11 18:52:59 +0100

    all schema added

---
+++ shareme_backend/schemas/comment.js
@@ -0,0 +1,22 @@
+export default{
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },           
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string'
+        },      
+    ]   
+}
